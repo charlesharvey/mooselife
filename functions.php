c91e53350a4f696f81b06c304d1a3829d59cbee4
Charles Harvey <harvey.charles@gmail.com>
2019-02-15 15:17:18 +0100

    add default post order of date

--- functions.php
+++ functions.php
@@ -700,5 +700,17 @@ function locations_for_map(){
     }
 
 
+    add_filter('pre_get_posts', 'set_post_order_in_admin', 5 );
+    function set_post_order_in_admin( $wp_query ) {
+        global $pagenow;
+        if ( is_admin() && 'edit.php' == $pagenow && !isset($_GET['orderby'])) {
+            $wp_query->set( 'orderby', 'date' );
+            $wp_query->set( 'order', 'DESC' );
+        }
+    }
+
+
+
+
 
     ?>
