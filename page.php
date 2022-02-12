88966f1c92db7401a44ffb333f67b669d289f1bf
Charles Harvey <harvey.charles@gmail.com>
2022-02-12 09:38:41 +0100

    use option page to get current trip

--- page.php
+++ page.php
@@ -1,39 +1,39 @@
 <?php get_header(); ?>
 
 
-<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<?php
- $loc_args = array(
-    'post_type' => 'location',
-    'posts_per_page' => -1,
-    'order' => 'DESC',
-    'orderby' => 'date'
-  );
-
-  if (isset($_GET['all_trips'])) :
-  else:
-    $loc_args['tax_query'] = array(
-      array(
-          'taxonomy' => 'trip',
-          'field' => 'slug',
-          'terms' => 'trip-2'
-      )
+    <?php
+    $loc_args = array(
+      'post_type' => 'location',
+      'posts_per_page' => -1,
+      'order' => 'DESC',
+      'orderby' => 'date'
     );
-  endif;
- ?>
+
+    if (isset($_GET['all_trips'])) :
+    else :
+      $loc_args['tax_query'] = array(
+        array(
+          'taxonomy' => 'trip',
+          'field' => 'term,_id',
+          'terms' => get_field('current_trip', 'option')
+        )
+      );
+    endif;
+    ?>
     <?php $locations = get_posts($loc_args); ?>
-<?php get_template_part('partials/locations'); ?>
+    <?php get_template_part('partials/locations'); ?>
 
 
 
-<?php endwhile; ?>
+  <?php endwhile; ?>
 
-<?php else: ?>
+<?php else : ?>
 
-    <article class="container">
-        <h2><?php _e('Sorry, nothing to display.', 'webfactor'); ?></h2>
-    </article>
+  <article class="container">
+    <h2><?php _e('Sorry, nothing to display.', 'webfactor'); ?></h2>
+  </article>
 
 <?php endif; ?>
 
