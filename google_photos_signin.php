55dfe780bdcb235d77d1edb4937f42c0e1e13189
Charles Harvey <harvey.charles@gmail.com>
2022-03-17 12:22:06 +0100

    moop

--- google_photos_signin.php
+++ google_photos_signin.php
@@ -43,6 +43,12 @@ if (!isset($_GET['code'])) {
     $authToken = $oauth2->fetchAuthToken();
     $refreshToken = $authToken['access_token'];
 
+
+    var_dump($oauth2);
+    var_dump($clientId);
+    var_dump($clientSecret);
+    var_dump($refreshToken);
+
     // The UserRefreshCredentials will use the refresh token to 'refresh' the credentials when
     // they expire.
     $_SESSION['credentials'] = new UserRefreshCredentials(
@@ -55,5 +61,5 @@ if (!isset($_GET['code'])) {
     );
 
     // Return the user to the home page.
-    header("Location: google_photos_index.php");
+    // header("Location: google_photos_index.php");
 }
