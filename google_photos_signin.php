afc8821804a57bfa54a8fb3135a659e38b05a471
Charles Harvey <harvey.charles@gmail.com>
2022-03-17 11:58:44 +0100

    add google photos composer test

---
+++ google_photos_signin.php
@@ -0,0 +1,50 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use Google\Auth\Credentials\UserRefreshCredentials;
+use Google\Auth\OAuth2;
+
+
+$clientSecretJson = json_decode(
+    file_get_contents('../../client_secret.json'),
+    true
+)['web'];
+$clientId = $clientSecretJson['client_id'];
+$clientSecret = $clientSecretJson['client_secret'];
+
+$oauth2 = new OAuth2([
+    'clientId' => $clientId,
+    'clientSecret' => $clientSecret,
+    'authorizationUri' => 'https://accounts.google.com/o/oauth2/v2/auth',
+    // Where to return the user to if they accept your request to access their account.
+    // You must authorize this URI in the Google API Console.
+    'redirectUri' => $redirectURI,
+    'tokenCredentialUri' => 'https://www.googleapis.com/oauth2/v4/token',
+    'scope' => array('photoslibrary.sharing'),
+]);
+
+// The authorization URI will, upon redirecting, return a parameter called code.
+if (!isset($_GET['code'])) {
+    $authenticationUrl = $oauth2->buildFullAuthorizationUri(['access_type' => 'offline']);
+    header("Location: " . $authenticationUrl);
+} else {
+    // With the code returned by the OAuth flow, we can retrieve the refresh token.
+    $oauth2->setCode($_GET['code']);
+    $authToken = $oauth2->fetchAuthToken();
+    $refreshToken = $authToken['access_token'];
+
+    // The UserRefreshCredentials will use the refresh token to 'refresh' the credentials when
+    // they expire.
+    $_SESSION['credentials'] = new UserRefreshCredentials(
+        $scopes,
+        [
+            'client_id' => $clientId,
+            'client_secret' => $clientSecret,
+            'refresh_token' => $refreshToken
+        ]
+    );
+
+    // Return the user to the home page.
+    header("Location: google_photos_index.php");
+}
